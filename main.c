b8856c12e6b3c0ba8b68b949c3f7887524fdf5b9
Mikael Berglund <mberglun@student.hive.fi>
2019-11-05 20:02:04 +0200

    preliminary test

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: mberglun <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/05 19:19:30 by mberglun          #+#    #+#             */
-/*   Updated: 2019/11/05 19:46:30 by mberglun         ###   ########.fr       */
+/*   Updated: 2019/11/05 20:00:52 by mberglun         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,17 @@
 
 int	main(void)
 {
-	char	*line = ft_strnew(12);
-	int res = get_next_line(123132, &line);
-	printf("%d\n", res);
+	int		fd;
+	char	*cline;
+
+	fd = open("./Makefile", O_RDONLY);
+	if (fd < 0)
+	{
+		printf("Unable to open file for testing\n");
+		return (0);
+	}
+
+	get_next_line(fd, &cline);
+	printf("%s\n", cline);
 	return (0);
 }
