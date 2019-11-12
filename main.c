e2099573f1243fa3726efc102fc7ab7a7bb3c273
mickberg <mikael.berglund2@gmail.com>
2019-11-12 10:13:10 +0200

    test for stdin

--- main.c
+++ main.c
@@ -6,27 +6,35 @@
 /*   By: mberglun <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/05 19:19:30 by mberglun          #+#    #+#             */
-/*   Updated: 2019/11/11 21:07:48 by mikaelber        ###   ########.fr       */
+/*   Updated: 2019/11/12 10:06:05 by mikaelber        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "testlib.h"
 
-int	main(void)
+int	main(int argc, char **argv)
 {
 	int		fd;
 	char	*cline;
 
-	fd = open("./Makefile", O_RDONLY);
-	if (fd < 0)
+	if (argc < 2)
 	{
-		printf("Unable to open file for testing\n");
-		return (0);
+		fd = 0;
+	}
+	else
+	{
+		fd = open(argv[1], O_RDONLY);
+		if (fd < 0)
+		{
+			ft_putstr("Unable to open file for testing\n");
+			return (0);
+		}
 	}
 
 	while (get_next_line(fd, &cline))
 	{
 		printf("%s\n", cline);
 	}
+
 	return (0);
 }
