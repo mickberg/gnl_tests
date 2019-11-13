020b2596637d51b0e73af07b2c3e69e7e0c00139
Mikael Berglund <mberglun@student.hive.fi>
2019-11-13 19:55:54 +0200

    add option to read from 2 files simultaniously

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: mberglun <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/05 19:19:30 by mberglun          #+#    #+#             */
-/*   Updated: 2019/11/12 10:06:05 by mikaelber        ###   ########.fr       */
+/*   Updated: 2019/11/13 19:54:35 by mberglun         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,26 +14,38 @@
 
 int	main(int argc, char **argv)
 {
-	int		fd;
+	int		fd[10];
+	int		fdi;
 	char	*cline;
 
 	if (argc < 2)
 	{
-		fd = 0;
+		fd[0] = 0;
 	}
-	else
+	else if (argc > 1)
 	{
-		fd = open(argv[1], O_RDONLY);
-		if (fd < 0)
+		fdi = 0;
+		while (fdi < argc -1)
 		{
-			ft_putstr("Unable to open file for testing\n");
-			return (0);
+			fd[fdi] = open(argv[fdi + 1], O_RDONLY);
+			if (fd[fdi] < 0)
+			{
+				printf("Unable to open file for testing: %s\n", argv[fdi + 1]);
+				return (0);
+			}
+			++fdi;
 		}
 	}
 
-	while (get_next_line(fd, &cline))
+	while (get_next_line(fd[0], &cline))
 	{
 		printf("%s\n", cline);
+		if (fd[1])
+		{
+			get_next_line(fd[1], &cline);
+			printf("%s\n", cline);
+		}
+
 	}
 
 	return (0);
